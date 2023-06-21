cb34797fde80da347968a14ff922f81ffb1ea549
Sam Johnson <s_johnso@yahoo.com>
2023-06-21 16:53:27 -0500

    Finished all questions

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -25,6 +25,7 @@ console.log(removedSupply);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25);
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,16 +33,40 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
+for (i = 0; i < supplyChanges.length; i++) {
+    if (supplyChanges[i] > 0) {
+        console.log(`Added ${supplyChanges[i]} parts.`);
+    } else if (supplyChanges[i] === 0) {
+        console.log('No change');
+    } else if (supplyChanges[i] < 0) {
+        console.log(`Removed ${supplyChanges[i] * -1} parts.`);
+    }
+}
+
 
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (entry of supplyChanges) {
+    if (entry > 0) {
+        console.log(`Added ${entry} parts.`);
+    } else if (entry === 0) {
+        console.log('No change');
+    } else if (entry < 0) {
+        console.log(`Removed ${entry * -1} parts.`);
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
+let totalSupplies = 0;
 console.log('8. Total supplies available is:');
+for (entry of supplyChanges) {
+    totalSupplies = totalSupplies + entry;
+}
+console.log(totalSupplies);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -49,4 +74,13 @@ console.log('8. Total supplies available is:');
 //    Use a `while` loop to keep adding parts to boxes until
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
+let parts = 572
+let boxes = 0
 console.log('9. Filling boxes with a "while" loop');
+while (parts >= 7) {
+    boxes++
+    parts = parts - 7
+}
+
+console.log(`The number of filled boxes is: ${boxes}`);
+console.log(`There are ${parts} remaining parts.`);
